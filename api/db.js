1d3a9cd3ad82f1678a8bd233c2ff48e8e3917e7d
Chamsou Mekahal <mekahalc@gmail.com>
2024-02-10 12:05:09 +0100

    third commit

--- api/db.js
+++ api/db.js
@@ -1,8 +1,9 @@
 // db.js
 const mysql = require("mysql");
+require("dotenv").config();
 
 const urlDB = `mysql://${process.env.MYSQL_ADDON_USER}:${process.env.MYSQL_ADDON_PASSWORD}@${process.env.MYSQL_ADDON_HOST}:${process.env.MYSQL_ADDON_PORT}/${process.env.MYSQL_ADDON_DB}`;
-const connection = mysql.createConnection();
+const connection = mysql.createConnection(urlDB);
 connection.connect((err) => {
   if (err) {
     console.error("Database connection failed: " + err.stack);
